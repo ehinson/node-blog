40c3fe07b293c317d89812e23c636c9efdc4852f
Erin Hinson <erinh@xceleration.com>
2017-07-27 17:29:07 -0400

    Show categories

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const mongo = require('mongodb');
-var db = require('monk')('localhost/nodeblog');
+var monk = require('monk');
+var db = monk('mongodb://localhost/nodeblog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   db = req.db;
   var posts = db.get('posts');
-  posts.find({}, {}, (err, posts) => {
+  // posts.find({}, {}).then(function() {
+  //   res.render('index', { title: 'Node Express Blog', posts: posts });
+  // });
+  posts.find({}, {}, (err, post) => {
     if (err) {
       console.error(err);
+    } else {
+      res.render('index', { title: 'Node Express Blog', posts: post });
     }
-    res.render('index', { posts: posts, title: 'Node Express Blog' });
   });
 });
 
--- routes/posts.js
+++ routes/posts.js
@@ -3,10 +3,21 @@ var router = express.Router();
 const multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 
+const mongo = require('mongodb');
+var db = require('monk')('mongodb://localhost/nodeblog');
+
 /* GET posts/add listing. */
 router.get('/add', function(req, res, next) {
-  res.render('addpost', {
-    title: 'Add a Post'
+  var categories = db.get('categories');
+  categories.find({}, {}, (err, cat) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.render('addpost', {
+        title: 'Add a Post',
+        categories: cat
+      });
+    }
   });
 });
 
@@ -17,7 +28,34 @@ router.post('/add', upload.single('image--upload'), function(req, res, next) {
   var category = req.body.category;
   var body = req.body.body;
   var date = new Date();
-  console.log(req.body);
+  if (req.file) {
+    var mainimage = req.file.filename;
+  } else {
+    var mainimage = 'no-image.jpg';
+  }
+
+  //Validation
+  req.checkBody('title', 'Title is required').notEmpty();
+  req.checkBody('body', 'A message is required').notEmpty();
+
+  var errors = req.getValidationResult().then(result => {
+    if (!result.isEmpty()) {
+      res.render('addpost', {
+        errors
+      });
+    } else {
+      var posts = db.get('posts');
+      posts.insert({ title: title, author: author, category, date, mainimage }, (err, post) => {
+        if (err) {
+          res.send(err);
+        } else {
+          req.flash('success', 'Post Added');
+          res.location('/');
+          res.redirect('/');
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
